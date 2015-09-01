b714ec25dc31387e9e89f17dd934087c8ca61e53
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-09-01 16:24:41 +0200

    Updated service layer and dao version

--- service/src/main/java/no/imr/nmdapi/nmdechosounder/service/NMDEchosounderServiceImpl.java
+++ service/src/main/java/no/imr/nmdapi/nmdechosounder/service/NMDEchosounderServiceImpl.java
@@ -1,7 +1,8 @@
 package no.imr.nmdapi.nmdechosounder.service;
 
-import no.imr.nmdapi.dao.file.NMDDataDao;
+import no.imr.nmdapi.dao.file.NMDDatasetDao;
 import no.imr.nmdapi.generic.nmdechosounder.domain.luf20.EchosounderDatasetType;
+import org.apache.commons.configuration.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -11,45 +12,60 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class NMDEchosounderServiceImpl implements NMDEchosounderService {
 
+    /**
+     * Data type.
+     */
+    private static final String TYPE = "echosounder";
+
+    /**
+     * Dataset name.
+     */
+    private static final String DATASET_NAME = "data";
+
     @Autowired
-    private NMDDataDao nmdDataDao;
+    private NMDDatasetDao nmdDatasetDao;
+
+    @Autowired
+    private Configuration configuration;
 
     @Override
     public Object getData(final String missiontype, final String year, final String platform, final String delivery) {
-        return nmdDataDao.get(missiontype, year, platform, delivery, EchosounderDatasetType.class);
+        return nmdDatasetDao.get(TYPE, DATASET_NAME, EchosounderDatasetType.class.getPackage().getName(), missiontype, year, platform, delivery);
     }
 
     @Override
     public void deleteData(final String missiontype, final String year, final String platform, final String delivery) {
-        nmdDataDao.delete(missiontype, year, platform, delivery);
-        nmdDataDao.deleteDataset(missiontype, year, platform, delivery, "ECHOSOUNDER");
+        nmdDatasetDao.delete(TYPE, DATASET_NAME, true, missiontype, year, platform, delivery);
     }
 
    @Override
     public void insertData(final String missiontype, final String year, final String platform, final String delivery, final EchosounderDatasetType dataset) {
-        nmdDataDao.insert(missiontype, year, platform, delivery, dataset, EchosounderDatasetType.class);
-        nmdDataDao.insertDataset(missiontype, year, platform, delivery, "ECHOSOUNDER");
+        String readRole = configuration.getString("default.readrole");
+        String writeRole = configuration.getString("default.writerole");
+        String owner = configuration.getString("default.owner");
+        nmdDatasetDao.insert(writeRole, readRole, owner, TYPE, DATASET_NAME, dataset, true, missiontype, year, platform, delivery);
     }
 
 
     @Override
     public void updateData(final String missiontype, final String year, final String platform, final String delivery, final EchosounderDatasetType dataset) {
-        nmdDataDao.update(missiontype, year, platform, delivery, dataset, EchosounderDatasetType.class);
+        nmdDatasetDao.update(TYPE, DATASET_NAME, dataset, missiontype, year, platform, delivery);
     }
 
     @Override
     public boolean hasData(String missiontype, String year, String platform, String delivery) {
-        return nmdDataDao.hasData(missiontype, year, platform, delivery);
+        return nmdDatasetDao.hasData(TYPE, DATASET_NAME, missiontype, year, platform, delivery);
+    }
+
+    @Override
+    public Object getDataByCruiseNr(final String cruisenr) {
+        return nmdDatasetDao.getByCruisenr(TYPE, DATASET_NAME, EchosounderDatasetType.class.getPackage().getName(), cruisenr);
     }
 
     @Override
     public boolean hasDataByCruiseNr(final String cruisenr) {
-        return nmdDataDao.hasDataByCruiseNr(cruisenr);
+        return nmdDatasetDao.hasDataByCruisenr(TYPE, DATASET_NAME, EchosounderDatasetType.class.getPackage().getName(), cruisenr);
     }
 
-    @Override
-    public Object getDataByCruiseNr(String cruisenr) {
-        return nmdDataDao.getByCruiseNr(EchosounderDatasetType.class, cruisenr);
-    }
 
 }
